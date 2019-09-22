cd0c65457c12c71c00ddfb44c4558a0804036b2b
Sahil Kamesh <skamesh2@illinois.edu>
2019-09-22 04:48:16 -0500

    Updates

---
+++ precache-manifest.b9a266ab3e559ba6eb86742d4b704e19.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03561d56cb79290cac20fefa97a5499f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "48fd325ec491ba9c8901",
+    "url": "/react-gh-pages/static/css/main.e26504bc.chunk.css"
+  },
+  {
+    "revision": "57f9d778efbde3af0796",
+    "url": "/react-gh-pages/static/js/2.f1f68f98.chunk.js"
+  },
+  {
+    "revision": "48fd325ec491ba9c8901",
+    "url": "/react-gh-pages/static/js/main.58f32814.chunk.js"
+  },
+  {
+    "revision": "5c14bf8d78ce841fcbb2",
+    "url": "/react-gh-pages/static/js/runtime-main.881c30c4.js"
+  },
+  {
+    "revision": "bd555f22ffae5f7e1b364ffa79acd278",
+    "url": "/react-gh-pages/static/media/HI_logo.bd555f22.png"
+  },
+  {
+    "revision": "413893b7dccf69f3bff496485d8d20a7",
+    "url": "/react-gh-pages/static/media/facebook.413893b7.413893b7.svg"
+  },
+  {
+    "revision": "c119364948d3b16d1c773976a766c65b",
+    "url": "/react-gh-pages/static/media/github.c1193649.c1193649.svg"
+  },
+  {
+    "revision": "8b73ec2e84d9ef4ca783ac34063e8b14",
+    "url": "/react-gh-pages/static/media/instagram.8b73ec2e.8b73ec2e.svg"
+  },
+  {
+    "revision": "29663de06ac6952dd93751253e63d8f5",
+    "url": "/react-gh-pages/static/media/twitter.29663de0.29663de0.svg"
+  }
+]);
